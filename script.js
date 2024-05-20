778e996983990120cc573b585d362a245b61ea0f
manojCodes77 <147693594+manojCodes77@users.noreply.github.com>
2024-05-20 13:50:04 +0530

    Update script.js
    better functioning of start and end button

--- script.js
+++ script.js
@@ -6,17 +6,20 @@ function getRandomColor() {
     let letters = '0123456789ABCDEF';
     let color = '#';
     for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
+        color += letters[Math.floor(Math.random() * 16)];//we can also use ceil instead of floor to generate upper limit
     }
     return color;
 }
 
 startButton.addEventListener("click", function() {
-    intervalId = setInterval(function() {
-        document.body.style.backgroundColor = getRandomColor();
-    }, 500);
+    if(!intervalId) {
+        intervalId = setInterval(function() {
+            document.body.style.backgroundColor = getRandomColor();
+        }, 500);
+    }
 });
 
 stopButton.addEventListener("click", function() {
     clearInterval(intervalId);
+    intervalId = null;
 });
