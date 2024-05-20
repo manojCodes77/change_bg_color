51988f057a53a6fd7cc4f7d018fd69bedb661868
manojCodes77 <147693594+manojCodes77@users.noreply.github.com>
2024-05-20 13:01:46 +0530

    Add files via upload
    generating random colors using javascript

---
+++ script.js
@@ -0,0 +1,22 @@
+let startButton = document.getElementById("start");
+let stopButton = document.getElementById("stop");
+let intervalId;
+
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+startButton.addEventListener("click", function() {
+    intervalId = setInterval(function() {
+        document.body.style.backgroundColor = getRandomColor();
+    }, 1);
+});
+
+stopButton.addEventListener("click", function() {
+    clearInterval(intervalId);
+});
